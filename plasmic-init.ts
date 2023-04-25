68ac7c7a8cfe50afc9d381c21854ed7aab964d64
Salman Inayat <salmaninayat1999@gmail.com>
2023-04-25 19:46:33 +0500

    Plasmic demo updated

--- plasmic-init.ts
+++ plasmic-init.ts
@@ -8,9 +8,12 @@ export const PLASMIC = initPlasmicLoader({
     {
       // id: "8VtBq1uAF8x37koERPKh2w",
       // token: "7prlbmGvH364QS3dmQ94jLoCcW3tBOBSI9Dk1MLgNVeOiTgFG9ZL475o2s6kk9m1c2EHfYU6WByNkFAJqJg",
-      id: "aLmnRmimxnTz9jYhsdVbG1", // ID of a project you are using
+      // id: "aLmnRmimxnTz9jYhsdVbG1", // ID of a project you are using
+      // token:
+      //   "wg1sMNsklxuyTOfSHWSH1U7k4ul5dC40RuWgaVazEaEJklKjCgEclezGMCz4GVpt6Qa83dxDMSMzG4rsR4RQ", // API token for that project
+      id: "c7Q9C99zZ4cHRwZsjCNjsu", // ID of a project you are using
       token:
-        "wg1sMNsklxuyTOfSHWSH1U7k4ul5dC40RuWgaVazEaEJklKjCgEclezGMCz4GVpt6Qa83dxDMSMzG4rsR4RQ", // API token for that project
+        "Fa2VoR6g7DdSWWbhdUz3SMBWVBKwW4oLlTZzL2WV1LCoiu2RCQpM6Jm9ltGikdCjUKKkdfN53gGOrJudg", // API token for that project
     },
   ],
 
@@ -29,13 +32,13 @@ PLASMIC.registerComponent(PricingCodeComponent, {
   },
 });
 
-// PLASMIC.registerComponent(TestimonialCarousel, {
-//   name: "Testimonial Carousel",
-//   props: {
-//     verbose: "boolean",
-//     children: "slot",
-//   },
-// });
+PLASMIC.registerComponent(TestimonialCarousel, {
+  name: "Testimonial Carousel",
+  props: {
+    verbose: "boolean",
+    children: "slot",
+  },
+});
 
 // You can register any code components that you want to use here; see
 // https://docs.plasmic.app/learn/code-components-ref/
